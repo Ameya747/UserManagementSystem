1a60b158799d51432c523f6aca405d995742d318
Ameya747 <ameyakamble747@gmail.com>
2025-01-16 23:15:04 +0530

    comminting changes made in validation

--- Validations.java
+++ Validations.java
@@ -1,5 +1,26 @@
 package USM;
 
 public class Validations {
+	static Scanner Sc = new Scanner(System.in);
+	
+	public static String CheckValidUsername() {
+		return null;
+	}
+	
+	public static long checkContactNo() {
+		return 0;
+	} 
+	
+	public static String CheckPanCardNo() {
+		return null;
+	}
+	
+	public static String CheckAdhaarcardNo() {
+		return null;		
+	}
+	
+	public static String CheckEmailid() {
+		return null;
+	}
 	
 }
